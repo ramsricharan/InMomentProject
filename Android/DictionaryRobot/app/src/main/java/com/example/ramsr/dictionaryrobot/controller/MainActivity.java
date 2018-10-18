76e4678c048a1f893110fee3f4b7b537001d6df3
Ram Sri Charan <ramsricharan@Rams-MacBook-Air.local>
2018-10-18 08:55:45 -0700

    Fixed minor bugs for Android app

--- Android/DictionaryRobot/app/src/main/java/com/example/ramsr/dictionaryrobot/controller/MainActivity.java
+++ Android/DictionaryRobot/app/src/main/java/com/example/ramsr/dictionaryrobot/controller/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements SearchEventInterf
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                keywordEditText.clearFocus();
                 if(!keywordEditText.getText().toString().trim().isEmpty())
                 {
                     checkAndSearch();
